e9eb21c2f6bca5f3d296f83fd6fb62119f266919
Edinsoncg <edinsonck@gmail.com>
2024-12-09 16:14:31 -0500

    Archivo coneccion base de datos

---
+++ src/infraestructure/database/connection.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'mysql',
+    logging: false,
+ });
+
+module.exports = sequelize;
