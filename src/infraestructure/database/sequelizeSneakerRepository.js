52fc815d03d5bdf96383b2ce9deac238d66f1a83
Edinsoncg <edinsonck@gmail.com>
2024-12-09 16:33:04 -0500

    Cambios en archivos y añadido de caperta infraestructure con los archivos model, connection y sequilizeSneakerRepository

---
+++ src/infraestructure/database/sequelizeSneakerRepository.js
@@ -0,0 +1,32 @@
+const SneakerRepository = require('../../domain/repositories/sneakerRepository');
+const SneakerModel = require('./models/sneakerModel');
+
+class SequelizeSneakerRepository extends SneakerRepository {
+    async getAll() {
+        return await SneakerModel.findAll();
+    }
+
+    async getById(id) {
+        return await SneakerModel.findByPk(id);
+    }
+
+    async create(sneaker) {
+        return await SneakerModel.create(sneaker);
+    }
+
+    async update(id, sneaker) {
+        const sneakerToUpdate = await SneakerModel.findByPk(id);
+        if (sneakerToUpdate) {
+            return await sneakerToUpdate.update(sneaker);
+        }
+    }
+
+    async delete(id) {
+        const sneakerToDelete = await SneakerModel.findByPk(id);
+        if (sneakerToDelete) {
+            return await sneakerToDelete.destroy();
+        }
+    }
+};
+
+module.exports = SequelizeSneakerRepository;
