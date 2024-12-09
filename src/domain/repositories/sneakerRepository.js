0ed06337240fbbdffb569100cbff4545c0113271
Derian_Rivera <derianxbox.12@gmail.com>
2024-12-09 12:12:13 -0500

    correccion al archivo sneakerRepository con los cambios propuestos

--- src/domain/repositories/sneakerRepository.js
+++ src/domain/repositories/sneakerRepository.js
@@ -1,21 +1,33 @@
 class SneakerRepository {
     async getAll() {
         throw new Error('El método getAll no está implementado en SneakerRepository');
-    } 
+    }
 
     async getById(id) {
+        if (!id) {
+            throw new Error('El parámetro id es obligatorio para getById');
+        }
         throw new Error('El método getById no está implementado en SneakerRepository');
     }
 
     async create(sneaker) {
+        if (!sneaker || typeof sneaker !== 'object') {
+            throw new Error('Se requiere un objeto sneaker válido para create');
+        }
         throw new Error('El método create no está implementado en SneakerRepository');
     }
 
     async update(id, sneaker) {
+        if (!id || !sneaker || typeof sneaker !== 'object') {
+            throw new Error('Se requieren id y un objeto sneaker válido para update');
+        }
         throw new Error('El método update no está implementado en SneakerRepository');
     }
 
     async delete(id) {
+        if (!id) {
+            throw new Error('El parámetro id es obligatorio para delete');
+        }
         throw new Error('El método delete no está implementado en SneakerRepository');
     }
 }
