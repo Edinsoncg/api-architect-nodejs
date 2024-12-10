74ae6be2bf0f9d652f855b6bbb86533ccf8f68ef
Derian_Rivera <derianxbox.12@gmail.com>
2024-12-10 13:23:58 -0500

    Actualización del archivo en submódulo de la carpeta routes

---
+++ src/interfaceAdapters/routes/sneakerRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const SequelizeSneakerRepository = require('../../insfrastructure/database/SequelizeSneakerRepository');
+const sneakerController = require('../controllers/SneakerController');
+const validateData= require('../../utils/validateData');
+
+const router = express.Router();
+const sneakerRepository = new SequelizeSneakerRepository();
+const controller = sneakerController(sneakerRepository);
+
+router.get('/', controller.getAll);
+router.get('/:id', controller.getById);
+router.post('/',validateData, controller.create);
+router.put('/:id',validateData, controller.update);
+router.delete('/:id', controller.delete);
+
+module.exports = router;
